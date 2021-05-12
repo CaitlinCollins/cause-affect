969a4ac18ff63aae04e1dc548d3a9d285b15fe06
amin bashir <aminbashir65@gmail.com>
2021-05-11 21:21:13 -0500

    login/register

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from './components/Home';
 import Search from './components/Search';
 import Register from './components/Register';
 import MyCal from './components/MyCal';
+import Login from './components/Login';
         
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route exact path='/Search' component={Search} />
       <Route exact path='/Register' component={Register} />
       <Route exact path='/MyCal' component={MyCal} />
+      <Route exact path='/Login' component={Login} />
       <Footer />
     </HashRouter>
   );
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,8 +1,33 @@
 import React from "react";
 import { Container, Row, Col } from "../Grid";
+import "./style.css";
 
 function Login() {
-
+return(
+    <Container> 
+<Row>
+  <Col size="md-12">
+    <div className="login-form">
+      <h2 className="page-title">Login</h2>
+      <form className="form">
+        <div className="form-group loginInput">
+          <label for="email-login">Email:</label>
+          <input className="form-input inputLogin" type="text" id="email-login" />
+        </div>
+        <div className="form-group loginInput">
+          <label for="password-login">Password:</label>
+          <input className="form-input inputLogin" type="password" id="password-login" />
+        </div>
+        <div className="form-group">
+          <button className="btn loginBtn" type="submit">Login!</button>
+        </div>
+      </form>
+    </div>
+  </Col>
+  </Row>
+    </Container>
+    
+)
 
 }
 
