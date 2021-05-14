1e9af4fcdd70a7a170bd75666715b18e6b13c408
Frank Merchlewitz <jlimhb@gmail.com>
2021-05-13 21:29:51 -0500

    updating rendered api files

--- client/src/components/Result/index.js
+++ client/src/components/Result/index.js
@@ -1,9 +1,17 @@
-import React from "react";
-import { Container, Row, Col } from "../Grid";
+import React from 'react';
 
-function Result() {
-
-
+const Result = ({ cause }) => {
+    return (
+        <div>
+            <div>
+                <h1>
+                    <a 
+                    href={cause.websiteURL} target="_blank"> {cause.charityName}</a>
+                </h1>
+                <h3>{cause.mission}</h3>
+            </div>
+        </div>
+    )
 }
 
 export default Result;
