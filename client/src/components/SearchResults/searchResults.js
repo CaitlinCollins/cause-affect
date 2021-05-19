0dc485d1d60a0192eab903237cd872db8b5884d7
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-05-18 19:54:19 -0500

    Search Results and MyCal page styling

--- client/src/components/SearchResults/searchResults.js
+++ client/src/components/SearchResults/searchResults.js
@@ -1,9 +1,59 @@
 import React from "react";
-import { Container, Row, Col } from "../Grid";
+import "./searchResults.scss";
 
 function SearchResults() {
-
-
+    return (
+    <div>
+    <div id="search-results-div" className="card text-center">
+        <h2 id="org-name">My Organization</h2>
+            <img id="org-img"
+                alt="Organization Photo"
+                src="/assets/cause.jpeg"
+            ></img>
+            <ul id="org-details">
+                <p>Details: These are details!</p>
+                <p>Website: <a href="#" target="_blank" id="website">www.mywebsite.com</a></p>
+                <p>Date: Today!</p>
+                <p>Time: Now!</p>
+                <p>Volunteers Needed: 10000</p>
+            </ul>
+            <button id="org-volunteer">Volunteer Now!</button>
+            <button id="org-donate">Make a Donation!</button>
+    </div>
+    <div id="search-results-div" className="card text-center">
+    <h2 id="org-name">My Organization</h2>
+        <img id="org-img"
+            alt="Organization Photo"
+            src="/assets/cause.jpeg"
+        ></img>
+        <ul id="org-details">
+            <p>Details: These are details!</p>
+            <p>Website: <a href="#" target="_blank" id="website">www.mywebsite.com</a></p>
+            <p>Date: Today!</p>
+            <p>Time: Now!</p>
+            <p>Volunteers Needed: 10000</p>
+        </ul>
+        <button id="org-volunteer">Volunteer Now!</button>
+        <button id="org-donate">Make a Donation!</button>
+    </div>
+    <div id="search-results-div" className="card text-center">
+        <h2 id="org-name">My Organization</h2>
+            <img id="org-img"
+                alt="Organization Photo"
+                src="/assets/cause.jpeg"
+            ></img>
+            <ul id="org-details">
+                <p>Details: These are details!</p>
+                <p>Website: <a href="#" target="_blank" id="website">www.mywebsite.com</a></p>
+                <p>Date: Today!</p>
+                <p>Time: Now!</p>
+                <p>Volunteers Needed: 10000</p>
+            </ul>
+            <button id="org-volunteer">Volunteer Now!</button>
+            <button id="org-donate">Make a Donation!</button>
+        </div>
+        </div>
+    )
 }
 
 export default SearchResults;
