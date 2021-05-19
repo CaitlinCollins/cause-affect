a524fd20ccd2132884a442ea018db399f8437606
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-18 20:30:16 -0500

    updating search results

--- client/src/components/SearchResults/searchResults.js
+++ client/src/components/SearchResults/searchResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios'
-import Result from '../Result';
+import Result from '../Result/result';
 
 const SearchResults = ({ city, state, cause }) => {
 
