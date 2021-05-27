b65c295c8f3c81b994ac7e7adf4ccc1a876e1690
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-05-26 20:34:13 -0500

    update register org style

--- client/src/components/RegisterBtn/index.js
+++ client/src/components/RegisterBtn/index.js
@@ -3,9 +3,11 @@ import React from "react";
 // register user button
 export function RegisterBtn(props) {
   return (
+    
     <button {...props} className="btn loginBtn">
       {props.children}
     </button>
+    
   );
 }
 
--- client/src/components/RegisterOrgBtn/index.js
+++ client/src/components/RegisterOrgBtn/index.js
@@ -3,9 +3,11 @@ import React from "react";
 // register Organization button
 export function RegisterOrgBtn(props) {
   return (
+    
     <button {...props} className="btn loginBtn">
       {props.children}
     </button>
+   
   );
 }
 
